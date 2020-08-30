79d7781181eb91c7513342ea37e7eaea13ed2ebc
RadySonabu <ardynubanos@gmail.com>
2020-08-31 00:50:43 +0800

    Updates

---
+++ precache-manifest.4eb557a0e9e00cadc0aa0d9540db701d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5b0ed24ad521dff59f99f7fff64c8740",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d6f01ea6a63a77b2138d",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "425e2cb0e1684fd5210a",
+    "url": "/static/js/2.09d0f23c.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.09d0f23c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d6f01ea6a63a77b2138d",
+    "url": "/static/js/main.b1775762.chunk.js"
+  },
+  {
+    "revision": "e3473a344493b6ab086d",
+    "url": "/static/js/runtime-main.21e4bbbf.js"
+  }
+]);
